d2b02cbe8a055e86aa3c43f4be77d891ce50b1e1
Sidharth Sharma <sid@Sidharths-MacBook-Pro.local>
2017-06-24 19:37:55 -0700

    Initial iteration

---
+++ README.md
@@ -0,0 +1,3 @@
+## CodePath Prework - Tip Calculator 
+
+This is the first iteration of the TipCalculator app in Swift. I will be making more updates when I have some down time.
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,18 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var tipAmount: UISegmentedControl!
+    @IBOutlet weak var totalAmount: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBAction func tip(_ sender: Any) {
+        let tipPercent = [0.10,0.15,0.20,0.25] //tip percentages matching segmented control index
+        let bill = Double(totalAmount.text!) ?? 0 //gets the user input for bill amount (if no user input then automatically == 0
+        let tip = bill * tipPercent[tipAmount.selectedSegmentIndex] //assign the user selected tip value via segment control object
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.02f", tip)
+        totalLabel.text = String(format: "$%.02f", total)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let tapRecognizer = UITapGestureRecognizer() //For user's tap interaction
+        tapRecognizer.addTarget(self, action: #selector(ViewController.tappedOutside)) //Adds a target (ViewController) and an action (tappedOutside) to a gesture-recognizer object.
+        self.view.addGestureRecognizer(tapRecognizer) //add the Gesture recognizer to the UIView
     }
-
+    
+    func tappedOutside(){
+        self.view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
